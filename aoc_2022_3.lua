e6f94b434bc0f218a8933554e1aaf7dfdfd96825
Qvist30 <thomaskennedy85+git@gmail.com>
2022-12-03 23:09:40 -0500

    Still more updates

--- aoc_2022_3.lua
+++ aoc_2022_3.lua
@@ -1,4 +1,7 @@
-function lines_from(file)
+local file = 'params_3_2.txt'
+
+-- reads the file
+function read_file()
   local lines = {}
   for line in io.lines(file) do 
     lines[#lines + 1] = line
@@ -6,42 +9,50 @@ function lines_from(file)
   return lines
 end
 
--- tests the functions above
-local file = 'params_3_2.txt'
-local lines = lines_from(file)
-
-local totalSum = 0
+-- new code
 
 local priorityArray = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
-for k,v in pairs(lines) do
+
+-- Part 1 function
+function part_one(lines)
+  local totalSum = 0
+  for k,v in pairs(lines) do
   
-  local firstHalf = string.sub(v, 1, #v/2)
-  local secondHalf = string.sub(v, #v/2 + 1, #v)
-  local priority = 0;
-  for i=1, #firstHalf do
-    local c = string.sub(firstHalf, i, i)
-    if(string.match(secondHalf, c)) then
-      priority = string.find(priorityArray, c)
-      break
+    local firstHalf = string.sub(v, 1, #v/2)
+    local secondHalf = string.sub(v, #v/2 + 1, #v)
+    local priority = 0;
+    for i=1, #firstHalf do
+      local c = string.sub(firstHalf, i, i)
+      if(string.match(secondHalf, c)) then
+        priority = string.find(priorityArray, c)
+        break
+      end
     end
+    totalSum = totalSum + priority
   end
-  totalSum = totalSum + priority
+  return totalSum
 end
 
-print(totalSum)
-totalSum = 0
+-- Part 2 function
+function part_two(lines)
+  local totalSum = 0
+  for i =1, #lines,3 do
+    local priority = 0
+    local str1, str2, str3 = lines[i], lines[i+1], lines[i+2]
+    for i=1, #str1 do
+      local c = string.sub(str1, i, i)
+      if(string.match(str2, c) and string.match(str3, c)) then
+        priority = string.find(priorityArray, c)
+        break
+      end
+    end
+    totalSum = totalSum + priority
+  end
+  return totalSum
+end
+
+
+local lines = read_file()
+print(part_one(lines))
 print('------------------')
-for i =1, #lines,3 do
-  local priority = 0
-  local str1, str2, str3 = lines[i], lines[i+1], lines[i+2]
-  for i=1, #str1 do
-    local c = string.sub(str1, i, i)
-    if(string.match(str2, c) and string.match(str3, c)) then
-      priority = string.find(priorityArray, c)
-      break
-    end
-  end
-  totalSum = totalSum + priority
-end
-
-print(totalSum)
+print(part_two(lines))
